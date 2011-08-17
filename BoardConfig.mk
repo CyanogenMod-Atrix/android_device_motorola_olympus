3fd2529894d8ec810b29ad86a0e3e6881b964823
Koushik Dutta <koushd@gmail.com>
2011-08-17 16:43:34 -0700

    fixups for recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,3 +127,4 @@ BOARD_EGL_CFG := device/motorola/olympus/egl.cfg
 
 #USB Tethering
 BOARD_CUSTOM_USB_CONTROLLER := ../../device/motorola/olympus/UsbController.cpp
+BOARD_HAS_LARGE_FILESYSTEM := true
