fc3078337ee4705a7ad35817be646b64436ada17
Emilio López <turl@tuxfamily.org>
2011-11-01 16:23:26 +0000

    Enable BOARD_USE_MOTO_DOCK_HACK
    Change-Id: Id04778644ea4e3800ff7b9638da6d7dc0dce9ece

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,3 +129,6 @@ BOARD_EGL_CFG := device/motorola/olympus/config/egl.cfg
 #USB Tethering
 BOARD_CUSTOM_USB_CONTROLLER := ../../device/motorola/olympus/UsbController.cpp
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Dock Audio
+BOARD_USE_MOTO_DOCK_HACK := true
