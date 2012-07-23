29757112d6310466e6edf0f1acc370c67eb5311e
Emerson Pinter <dev@pinter.com.br>
2012-07-23 11:40:59 -0300

    Added storage option to OlympusParts and changed hdmiaudio summary

--- OlympusParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ OlympusParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -7,10 +7,18 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.util.Log;
+import android.os.SystemProperties;
 
 public class DeviceSettings extends PreferenceActivity implements OnPreferenceChangeListener {
 
-    private CheckBoxPreference hdmiAudioPref;
+    private CheckBoxPreference mHdmiAudioPref=null;
+    private CheckBoxPreference mSwitchStoragePref=null;
+
+    private static final String TAG = "OlympusParts";
+    private static final String KEY_SWITCH_STORAGE = "key_switch_storage";
+    private static final String KEY_HDMI_AUDIO = "hdmi_audio";
+    private static final String ACTION_AUDIO_DIGITAL="com.cyanogenmod.dockaudio.ENABLE_DIGITAL_AUDIO";
+    private static final String ACTION_AUDIO_SPEAKER="com.cyanogenmod.dockaudio.ENABLE_SPEAKER_AUDIO";
 
     /** Called when the activity is first created. */
     @Override
@@ -18,23 +26,31 @@ public class DeviceSettings extends PreferenceActivity implements OnPreferenceCh
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.main_preferences);
 
-        hdmiAudioPref = (CheckBoxPreference) getPreferenceScreen().findPreference("hdmi_audio");
-        hdmiAudioPref.setOnPreferenceChangeListener(this);
+        mHdmiAudioPref = (CheckBoxPreference) getPreferenceScreen().findPreference(KEY_HDMI_AUDIO);
+        mHdmiAudioPref.setOnPreferenceChangeListener(this);
+        mSwitchStoragePref = (CheckBoxPreference) getPreferenceScreen().findPreference(KEY_SWITCH_STORAGE);
+        mSwitchStoragePref.setChecked((SystemProperties.getInt("persist.sys.vold.switchexternal", 0) == 1));
+        mSwitchStoragePref.setOnPreferenceChangeListener(this);
+        if (SystemProperties.get("ro.vold.switchablepair","").equals("")) {
+            mSwitchStoragePref.setSummary(R.string.storage_switch_unavailable);
+            mSwitchStoragePref.setEnabled(false);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == hdmiAudioPref) {
+        if (preference == mHdmiAudioPref) {
             /* switch the audio state accordingly */
+            String audioAction=((Boolean) newValue)?ACTION_AUDIO_DIGITAL:ACTION_AUDIO_SPEAKER;
+            Log.d(TAG,"Sending intent with action "+audioAction);
             Intent audio = new Intent();
-            if ((Boolean) newValue) {
-                audio.setAction("com.cyanogenmod.dockaudio.ENABLE_DIGITAL_AUDIO");
-            } else {
-                audio.setAction("com.cyanogenmod.dockaudio.ENABLE_SPEAKER_AUDIO");
-            }
+            audio.setAction(audioAction);
             sendBroadcast(audio);
         }
-
+        if(preference == mSwitchStoragePref) {
+            Log.d(TAG,"Setting persist.sys.vold.switchexternal to "+(mSwitchStoragePref.isChecked() ? "1" : "0"));
+            SystemProperties.set("persist.sys.vold.switchexternal", ((Boolean) newValue) ? "1" : "0");
+        }
         return true;
     }
 }
