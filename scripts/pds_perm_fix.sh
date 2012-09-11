f72da10d1a91c7ebb96d3f8c2dae0d04ea272e4c
Emerson Pinter <dev@pinter.com.br>
2012-09-11 20:03:12 -0300

    Changes for cryptfs
    Moved all /data directories operation to post-fs-data.
    Added encryptable=footer to fstab.
    pds_perm_fix.sh now copy pds.img to /dev and not to /data (so we can
    unmount /data with "enablecrypto wipe").

--- scripts/pds_perm_fix.sh
+++ scripts/pds_perm_fix.sh
@@ -1,34 +1,25 @@
 #!/system/bin/sh
 export PATH=/system/xbin:$PATH
 
-if [ ! -f /data/pds-CM7.img ]
-then
-    #make a copy of pds in /data
-    dd if=/dev/block/mmcblk0p3 of=/data/pds-CM7.img
+#make a copy of pds in /dev
+dd if=/dev/block/mmcblk0p3 of=/dev/pds.img
 
-    #mount the fake pds
-    /system/xbin/losetup /dev/block/loop0 /data/pds-CM7.img
-    /system/xbin/busybox mount -o rw /dev/block/loop0 /pds
+#mount the fake pds
+/system/xbin/losetup /dev/block/loop0 /dev/pds.img
+/system/xbin/busybox mount -o rw /dev/block/loop0 /pds
 
-    cd /pds
-    #find and fix moto users first
-    /system/xbin/busybox find -user 9000 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9003 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9004 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9007 -exec chown 1000 {} \;
+cd /pds
+#find and fix moto users first
+/system/xbin/busybox find -user 9000 -exec chown 1000 {} \;
+/system/xbin/busybox find -user 9003 -exec chown 1000 {} \;
+/system/xbin/busybox find -user 9004 -exec chown 1000 {} \;
+/system/xbin/busybox find -user 9007 -exec chown 1000 {} \;
 
-    #find and fix moto grps
-    /system/xbin/busybox find -group 9000 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9003 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9004 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9007 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9009 -exec chgrp 1000 {} \;
+#find and fix moto grps
+/system/xbin/busybox find -group 9000 -exec chgrp 1000 {} \;
+/system/xbin/busybox find -group 9003 -exec chgrp 1000 {} \;
+/system/xbin/busybox find -group 9004 -exec chgrp 1000 {} \;
+/system/xbin/busybox find -group 9007 -exec chgrp 1000 {} \;
+/system/xbin/busybox find -group 9009 -exec chgrp 1000 {} \;
 
-    echo "Backed up, permission fixed and mounted PDS"
-else
-    #mount the fake pds
-    /system/xbin/losetup /dev/block/loop0 /data/pds-CM7.img
-    /system/xbin/busybox mount -o rw /dev/block/loop0 /pds
-
-    echo "Mounted PDS only."
-fi
+echo "Backed up, permission fixed and mounted PDS"
