a7a005767fe1e6ac17f424f0047a9975ba11d196
Emerson Pinter <dev@pinter.com.br>
2012-09-19 11:14:14 -0300

    Add manufacturer

--- cm.mk
+++ cm.mk
@@ -18,5 +18,7 @@ PRODUCT_NAME := cm_olympus
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := olympus
 
+PRODUCT_MANUFACTURER=motorola
+
 ## Device fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=MOTO PRODUCT_NAME=olyatt BUILD_PRODUCT=olyatt BUILD_FINGERPRINT=MOTO/olyatt/olympus:2.3.4/4.5.91/110625:user/release-keys PRIVATE_BUILD_DESC="olympus-user 2.3.4 4.5.91 110625 release-keys"
