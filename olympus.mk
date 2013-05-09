00ca18e7b824ddda13ea384eab570c7353705295
Emerson Pinter <dev@pinter.com.br>
2013-05-09 20:16:55 -0300

    Remove init.trace from device tree

--- olympus.mk
+++ olympus.mk
@@ -5,7 +5,6 @@ LOCAL_PATH := device/motorola/olympus
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/root/init.target.rc:root/init.target.rc \
-    $(LOCAL_PATH)/root/init.trace.rc:root/init.trace.rc \
     $(LOCAL_PATH)/root/init.target.usb.rc:root/init.target.usb.rc \
     $(LOCAL_PATH)/root/ueventd.olympus.rc:root/ueventd.olympus.rc \
     $(LOCAL_PATH)/root/fstab.olympus:root/fstab.olympus
